b9a42fb9836706061c5dbea5374968e3217bd432
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-02-03 09:52:05 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
 	$("#ptoggler").click(function(){
 		$(".dissapear").toggle();
 	});
-   var q;
-   for(q=1; q<4; q++)
+  
 
-	$("#q"+ q).click(function(){
-    $("#a"+ q).slideToggle("slow");
+ 	$("#quiz").click(function(event){
+          var number = event.target.id.slice(-1);
+    $("#a"+ number).slideToggle("slow");
   });
 });
